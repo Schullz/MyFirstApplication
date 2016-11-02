24e5c0a39c27c8a3cc722e6b8441e54267e52c5e
PavelDevelop <paveldsirius@gmail.com>
2016-11-02 23:13:01 +0300

    first commit

---
+++ MyFirstApplication/Controllers/CatsController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MyFirstApplication.Models;
+
+namespace MyFirstApplication.Controllers
+{
+    public class CatsController : Controller
+    {
+        // GET: Cats
+        public ActionResult Index()
+        {
+            var cats = new List<CatsViewModel>
+            {
+                new CatsViewModel
+                {
+                    Name = "Барсик",
+                    FeetCount = 4,
+                    IsLikeFish = true
+                }
+            };
+
+            return View(cats);
+        }
+
+        public ActionResult Create()
+        {
+            return View();
+        }
+    }
+}
